058f76065c79c182e010d262dae6b9c5e8c61599
midulislamrasel <midulraddy121@gmail.com>
2023-12-02 16:54:51 +0600

    postman

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
+//Middleware
+app.use(morgan("dev"));
+
 app.get("/test", (req, res) => {
-  res.send("Api is wroking fine");
+  res.send("Api is wroking fine ");
+});
+
+app.post("/test", (req, res) => {
+  res.send("post Api is wroking fine ");
+});
+
+app.put("/test", (req, res) => {
+  res.send("put Api is wroking fine ");
 });
 
 app.listen(3001, () => {
