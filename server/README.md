28abbf0b74bd4bf4180bdd698661e7cedc61ead4
midulislamrasel <midulraddy121@gmail.com>
2023-12-03 23:58:50 +0600

    Express Erro

--- server/README.md
+++ server/README.md
@@ -16,3 +16,6 @@
 4. HTTP request & response
 5. nodemon adn morgan package
 6. Api testing with postman
+7. Middleware & types of Middleware
+8. Express Error Handling Middleware
+9. How to handle HTTP error
--- server/src/server.js
+++ server/src/server.js
@@ -1,28 +1,43 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
 
 //Middleware
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const isLoggedIn = (req, res, next) => {
-  const login = false;
+  const login = true;
   if (login) {
     next();
   } else {
-    return res.status(401).json({ message: "Please Login First " });
+    return res.status(401).console.log("Please Login First ");
   }
 };
 
 // =======Api cll mi========
-app.get("/user", isLoggedIn, (req, res) => {
+app.get("/test", (req, res) => {
+  res.send("Api is wroking fine ");
+});
+
+app.get("/test/users", isLoggedIn, (req, res) => {
   res.status(200).send({
     message: "Usr progile is reutend",
   });
 });
 
-app.get("/test", (req, res) => {
-  res.send("Api is wroking fine ");
+//client error handling
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Rout not found" });
+  next();
+});
+
+//server error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something Broke");
 });
 
 app.listen(3001, () => {
